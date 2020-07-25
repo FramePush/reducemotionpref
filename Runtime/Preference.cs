2ee28f44eea27376798ee3f2ef454e0723aa1d16
Brian Turner <brianturner@framepush.com>
2020-07-25 06:01:23 -0400

    Initial Bare Package

---
+++ Runtime/Preference.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FramePush.ReduceMotionPref
+{
+    public enum Preference
+    {
+        NoPreference,
+        ReduceMotion
+    }
+}
